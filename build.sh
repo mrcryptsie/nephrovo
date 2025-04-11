b3239803a5d23d78cf768d98d04c1c528a6069ad
mrcryptsie <richardkeke15@gmail.com>
2025-04-11 10:42:26 +0100

    fddfdfd

--- build.sh
+++ build.sh
@@ -38,7 +38,7 @@ pip3 install -r server/fastapi/production_requirements.txt
 
 # Copie des fichiers nécessaires dans le répertoire de distribution
 echo -e "${GREEN}Copie des fichiers dans le répertoire de distribution...${NC}"
-cp -r client/dist dist/public
+cp -r dist dist/public
 mkdir -p dist/server
 cp -r server/fastapi dist/server/
 cp -r attached_assets dist/attached_assets
